ea3df0db8b74d42e4305517b0433ec71b69d4baa
Alla Holovko <allagol1205@gmail.com>
2024-01-19 23:52:33 +0200

    Проект для настройки цветовых оттенков монитора

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project';
+  backgroundColor: string = "#FFF";
+  step = 2;
+
+  getColor(i: number, j: number) {
+    switch (i) {
+      case 0: return `rgb(${255-j*this.step}, ${255-j*this.step}, ${255-j*this.step})`; // silver
+      case 1: return `rgb(255, ${255-j*this.step}, ${255-j*this.step})`;                // red
+      case 2: return `rgb(${255-j*this.step}, 255, ${255-j*this.step})`;                // green
+      case 3: return `rgb(${255-j*this.step}, ${255-j*this.step}, 255)`;                // blue
+      case 4: return `rgb(255, 255, ${255-j*this.step})`;                               // yellow
+      case 5: return `rgb(255, ${255-j*this.step}, 255)`;                               // violet
+      case 6: return `rgb(${255-j*this.step}, 255, 255)`;                               // cian
+      case 7: return `rgb(${j*this.step}, ${j*this.step}, ${j*this.step})`;             // silver
+      case 8: return `rgb(${j*this.step}, 0, 0)`;                                       // red
+      case 9: return `rgb(0, ${j*this.step}, 0)`;                                       // green
+     case 10: return `rgb(0, 0, ${j*this.step})`;                                       // blue
+     case 11: return `rgb(${j*this.step}, ${j*this.step}, 0)`;                          // yellow
+     case 12: return `rgb(${j*this.step}, 0, ${j*this.step})`;                          // violet
+     case 13: return `rgb(0, ${j*this.step}, ${j*this.step})`;                          // cian
+     default: return "#FFF";
+    }
+  }
+
+  click(i, j) {
+    this.backgroundColor = this.getColor(i, j);
+  }
 }
